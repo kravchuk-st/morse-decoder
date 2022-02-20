f04f1f47fd6a44a1fc451b93e6f8ff19936e48fc
kravchuk-st <57140996+kravchuk-st@users.noreply.github.com>
2022-02-20 15:37:05 +0200

    feat: add js file

--- src/index.js
+++ src/index.js
@@ -38,7 +38,46 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let word = [];
+    let code = [];
+    let letter = [];
+    let arr = '';
+    let decode = '';
+
+    for(let i = 0; i < expr.length; i += 10) {
+        word.push(expr.slice(i, i + 10));
+    }
+
+    word.forEach(item => {
+        for(let i = 0; i < item.length; i += 2) {
+            code.push(item.slice(i, i + 2));
+        }
+    });
+
+    for(let i = 0; i < code.length; i++) {
+        switch(code[i]) {
+            case '10':
+                code[i] = '.';
+                break;
+            case '11':
+                code[i] = '-';
+                break;
+            case '00':
+                code[i] = ' ';
+                break;
+            case '**':
+                code[i] = '*';
+                break
+        }
+        arr += code[i];
+    }
+
+    for(let i = 0; i < arr.length; i += 5) {
+        letter.push(arr.slice(i, i + 5).trim());
+    }
+
+    decode = letter.join(' ').split(' ***** ').map(a => a.split(' ').map(b => MORSE_TABLE[b]).join('')).join('');
+    return decode;
 }
 
 module.exports = {
